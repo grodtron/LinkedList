ad0b6fc3f4ece8cd69bf2d4244ec082abfaa4618
Gordon Bailey <gordonpbailey@gmail.com>
2012-07-15 17:06:12 -0400

    Cleaned up main.cpp and added .gdbinit to .gitignore
    Nothing major

--- main.cpp
+++ main.cpp
@@ -7,39 +7,19 @@ using std::endl;
 int main()
 {
 
-   const int length = 256;
+   const int length = 2<<20;
 
    LinkedList<int> list(8);
 
    int i;
 
-   for(i = 0; i < (length/2); ++i){
+   for(i = 0; i < length; ++i){
       list.addFirst(i);
-   }
-
-   for(     ; i < length; ++i){
       list.addLast(i);
    }
 
    LinkedList<int>::iterator it = list.begin();
    LinkedList<int>::iterator end= list.end();
-
-   it = list.begin();
-
-   for(i = 0; i < 4; ++i) ++it;
-
-   list.remove(it);
-   list.remove(it);
-
-   it = list.begin();
-
-   list.addBefore(42, it);
-
-   list.addAfter(256, it);
-
-   for(it = list.begin(); it != end; ++it){
-      cout << *it << endl;
-   }
    
    return 0;
 }
